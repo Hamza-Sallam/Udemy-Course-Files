ce20db42f436a1ca673c1f50c54fb8064ff32e01
Hamza-Sallam <106164232+Hamza-Sallam@users.noreply.github.com>
2022-08-18 09:38:49 +0300

    files uploaded

---
+++ practice/hh.java
@@ -0,0 +1,14 @@
+public class hh{
+
+    public static void main(String[] args) {
+        int n=5;
+
+        for(int i=0; i<n;i++){
+            for(int j=0; j<n; j++){
+                 for(int k=i; k<j; k++){
+                    System.out.println("a");
+                }
+             }
+        }
+    }
+}
---
+++ tea project/hi.js
@@ -0,0 +1,7 @@
+
+try {
+    throw new Error();
+    console.log('Square');
+} catch(err) {
+    console.log('Circle');
+}
